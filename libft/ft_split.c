ebe408a4acf547180bffba3036d9882839c47a7e
AlaeMeddah <141645841+AlaeMeddah@users.noreply.github.com>
2025-11-27 17:05:12 +0100

    libft

---
+++ libft/ft_split.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almeddah <almeddah@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 12:29:59 by almeddah          #+#    #+#             */
+/*   Updated: 2024/11/21 18:07:53 by almeddah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_sep(char c, char sep)
+{
+	if (c == sep)
+		return (1);
+	return (0);
+}
+
+static int	count_words(const char *str, char sep)
+{
+	int	n;
+
+	n = 0;
+	while (is_sep(*str, sep) && *str)
+		str++;
+	while (*str)
+	{
+		while (!is_sep(*str, sep) && *str)
+			str++;
+		while (is_sep(*str, sep) && *str)
+			str++;
+		n++;
+	}
+	return (n);
+}
+
+static char	**ft_split2(const char *str, char **result, char sep)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (*str)
+	{
+		while (is_sep(*str, sep) && *str)
+			str++;
+		i = 0;
+		while (!is_sep(str[i], sep) && str[i])
+			i++;
+		if (*str)
+		{
+			result[j] = malloc(sizeof(char) * (i + 1));
+			if (!result[j])
+				return (0);
+			result[j][0] = '\0';
+			ft_strncat(result[j++], str, i);
+			str += i;
+		}
+	}
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		nb_words;
+	char	**result;
+
+	nb_words = count_words(s, c);
+	result = malloc(sizeof(char *) * (nb_words + 1));
+	if (!result)
+		return (0);
+	ft_split2(s, result, c);
+	result[nb_words] = 0;
+	return (result);
+}
